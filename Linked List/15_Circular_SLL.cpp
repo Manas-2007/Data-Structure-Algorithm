fa5c5d9aa75bcacd2a2fb6b79a8de9eec6da004e
Manas-2007 <patidarmanas01@gmail.com>
2026-03-26 01:23:04 +0530

    Insertion in SCLL

--- Linked List/15_Circular_SLL.cpp
+++ Linked List/15_Circular_SLL.cpp
@@ -40,22 +40,121 @@ void createList(Node *&head,int size)
 void traverse(Node *&head)
 {
     Node *ptr=head;
-    while(ptr->next!=head)
+    if(head==NULL)
     {
-        cout<<ptr->data<<"   ";
+        cout<<"List is Empty.....\n";
+    }
+    else
+    {
+        while(ptr->next!=head)
+        {
+            cout<<ptr->data<<"   ";
+            ptr=ptr->next;
+        }
+        cout<<ptr->data<<" ";
+    }
+}
+
+//Insert at ending
+void endinsert(Node *&head)
+{
+    Node*ptr=head;
+    int value;
+    cout<<"Enter the value of New Node : ";
+    cin>>value;
+    Node *newnode=new Node(value);
+    if(head==NULL)
+    {
+        head=newnode;
+        newnode->next=head;
+        ptr=head;
+        return;
+    }
+    else
+    {
+        while(ptr->next!=head)
+        {
+            ptr=ptr->next;
+        }
+        ptr->next=newnode;
+        newnode->next=head;
         ptr=ptr->next;
     }
-    cout<<ptr->data<<" ";
+}
+
+//Insert at Starting
+void insertstart(Node *&head)
+{
+    Node *ptr=head;
+    int value;
+    cout<<"Enter the value of New Node : ";
+    cin>>value;
+    Node *newnode=new Node(value);
+    if(head==NULL)
+    {
+        head=newnode;
+        newnode->next=head;
+        ptr=newnode;
+    }
+    else
+        {
+            while(ptr->next!=head)
+            {
+                ptr=ptr->next;
+            }
+            newnode->next=head;
+            ptr->next=newnode;
+            head=newnode;
+        }
+}
+
+//Insertion at nth position
+void insertposition(Node *&head,int position)
+{
+    Node *ptr=head;
+    int count=1,value;
+    cout<<"Enter the value of New Node : ";
+    cin>>value;
+    Node *newnode=new Node(value);
+    if(head==NULL)
+    {
+        head=newnode;
+        newnode->next=head;
+    }
+    else{
+        while(ptr->next!=head)
+        {
+            if(count==position)
+            {
+                newnode->next=ptr->next;
+                ptr->next=newnode;
+                break;
+            } 
+            count++;
+            ptr=ptr->next;
+        } 
+        if(ptr->next==head && count==position)
+        {
+            newnode->next=ptr->next;
+                ptr->next=newnode;
+        }
+
+    }
 }
 
 int main()
 {
-    int size;
+    int size,position;
     Node *head=NULL,*ptr;
     cout<<"Enter the size of List : ";
     cin>>size;
     createList(head,size);
     cout<<"     Traversing of Circular Linked List\n";
+    traverse(head);
+    cout<<"\n       Insertion at ending of the CSLL\n";
+    cout<<"Enter the Node Number after you want to Insert New Node : ";
+    cin>>position;
+   insertposition(head,position);
     traverse(head);
     return 0;
 }
