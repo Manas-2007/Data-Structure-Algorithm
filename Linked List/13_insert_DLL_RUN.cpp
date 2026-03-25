bd58ddb763eaecb4cd972e5390bd0d57d0bc825e
Manas-2007 <patidarmanas01@gmail.com>
2026-03-25 21:30:18 +0530

    Insertion in DLL

--- Linked List/13_insert_DLL_RUN.cpp
+++ Linked List/13_insert_DLL_RUN.cpp
@@ -1,5 +1,4 @@
 //Write a program to insert a node after nth node at runtime linked list creation...............
-
 #include<iostream>
 using namespace std;
 class Node{
@@ -15,12 +14,10 @@ class Node{
     }
 };
 //Creating linked list dynamically
-void CreateList(int size,Node *&head,Node*&ptr)
+void CreateList(int size,Node *&head)
 {
-    ptr=head;
+    Node *ptr=head;    
     int value;
-    cout<<"Enter the size of Linked List : ";
-    cin>>size;
     for(int i=0;i<size;i++)
     {
         cout<<"Value of NODE "<<i+1<<" : ";
@@ -40,9 +37,9 @@ void CreateList(int size,Node *&head,Node*&ptr)
     }
 }
 //Traversal of Linked List
-void Traversal(Node *&head,Node *&ptr)
+void Traversal(Node *&head)
 {
-    ptr=head;
+    Node *ptr=head;
     while(ptr!=NULL)
     {
         cout<<ptr->data<<"  ";
@@ -50,11 +47,56 @@ void Traversal(Node *&head,Node *&ptr)
     }
 }
 
+//Insertion of Node after given node
+void insertNode(Node *&head,int position)
+{
+    Node *ptr=head;
+    int count=1,value;
+    cout<<"Enter the value of New Node : ";
+    cin>>value;
+    Node *newnode=new Node(value);
+    while(ptr!=NULL && position>count)
+    {
+        count++;
+        ptr=ptr->next;
+    }
+    
+    //Checking status of previous node
+    if(ptr==NULL)
+    {
+        cout<<"Invalid position......\n";
+        return;
+
+    }
+   else if(ptr->next==NULL)                     //Insertion at last node
+    {
+        newnode->prev=ptr;
+        ptr->next=newnode;
+    }
+    else if(ptr->next!=NULL)
+    {
+        newnode->next=ptr->next;
+        newnode->next->prev=newnode;
+        ptr->next=newnode;
+        newnode->prev=ptr;
+    }
+       
+}
+
+
 int main()
 {
     Node *head=NULL,*ptr;
-    CreateList(5,head,ptr);
+    int size,position;
+    cout<<"Enter the size of Linked List : ";
+    cin>>size;
+    CreateList(size,head);
     cout<<"\n       YOUR LISTED DATA\n";
-    Traversal(head,ptr);
+    Traversal(head);
+    cout<<"\n   INSERTION OF NODE\n";
+    cout<<"Enter the Node after U want to insert : ";
+    cin>>position;
+    insertNode(head,position);
+    Traversal(head);
     return 0;
 }
